f61bddfd38c70e744309fc955265ff61541aa739
Weronika Puchała <ciemnamateria@gmail.com>
2023-05-09 17:55:04 +0200

    wrap function

---
+++ R/create_fit_dataset.R
@@ -0,0 +1,24 @@
+#' @export
+create_fit_dataset <- function(kin_dat,
+                               fit_k_params,
+                               control = list(maxiter = 1000,  scale = "levenberg"),
+                               trace = F,
+                               workflow = 321){
+
+  peptide_list <- kin_dat %>%
+    select(Sequence, Start, End) %>%
+    unique(.)
+
+  apply(peptide_list, 1, function(peptide){
+
+    filter(kin_dat, Sequence == peptide[[1]],
+                      Start == as.numeric(peptide[[2]]),
+                      End == as.numeric(peptide[[3]])) %>%
+    get_fit_results(fit_dat = .,
+                    fit_k_params = fit_k_params,
+                    control = control,
+                    trace = trace,
+                    workflow = workflow)
+  }) %>% bind_rows()
+
+}
