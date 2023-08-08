5b2474a05b0a17966327f91861ab787a84d4d578
Weronika Puchała <ciemnamateria@gmail.com>
2023-08-08 16:06:11 +0200

    plot rewritten to white

--- R/plot_hires.R
+++ R/plot_hires.R
@@ -23,34 +23,91 @@ plot_hires <- function(hires_params){
 #'
 #' @export
 plot_hires_components <- function(hires_params,
+                                  white = T,
                                   fractional = F){
 
-  hires_components <- hires_params %>%
-    filter(is.na(class_name), !is.na(n_1)) %>%
-    mutate(n = n_1 + n_2 + n_3,
-           n_1_color = rgb(n_1/n, 0, 0),
-           n_2_color = rgb(0, n_2/n, 0),
-           n_3_color = rgb(0, 0, n_3/n))
+  protein_length = max(hires_params[["position"]])
 
   edgy_classes <- hires_params %>%
     filter(!is.na(class_name))
 
-  ggplot() +
-    geom_rect(data = hires_components, aes(xmin = position, xmax = position + 1,
-                                    ymin = 0, ymax = 1), fill = hires_components[["n_1_color"]]) +
-    geom_rect(data = hires_components, aes(xmin = position, xmax = position + 1,
-                                    ymin = 1, ymax = 2), fill = hires_components[["n_2_color"]]) +
-    geom_rect(data = hires_components, aes(xmin = position, xmax = position + 1,
-                                    ymin = 2, ymax = 3), fill = hires_components[["n_3_color"]]) +
-    geom_rect(data = edgy_classes, aes(xmin = position, xmax = position + 1,
-                                    ymin = 0, ymax = 3), fill = edgy_classes[["color"]]) +
-    theme_bw() +
-    labs(title = "Assigned class components on sequence",
-         x = "Position",
-         y = "") +
-    theme(axis.ticks.y = element_blank(),
-          axis.text.y = element_blank()) +
-    coord_cartesian(x = c(0, max(hires_params[["position"]])+1))
+  no_coverage <- hires_params %>%
+    filter(is.na(class_name), is.na(n_1))
+
+  color_grey = rep("#B8B8B8", nrow(no_coverage))
+
+  if(white){
+
+    hires_components <- hires_params %>%
+      filter(is.na(class_name), !is.na(n_1)) %>%
+      mutate(n = n_1 + n_2 + n_3,
+             n_1_alpha = case_when(
+               n_1/n > 1 ~ 1,
+               T ~ n_1/n),
+             n_2_alpha = case_when(
+               n_2/n > 1 ~ 1,
+               T ~ n_2/n),
+             n_3_alpha = case_when(
+               n_3/n > 1 ~ 1,
+               T ~ n_3/n))
+
+    color_red = rep("#FF0000", nrow(hires_components))
+    color_green = rep("#00FF00", nrow(hires_components))
+    color_blue = rep("#0000FF", nrow(hires_components))
+
+    ggplot() +
+      geom_rect(data = hires_components, aes(xmin = position, xmax = position + 1,
+                                             ymin = 0, ymax = 1), alpha = hires_components[["n_1_alpha"]], fill = color_red) +
+      geom_rect(data = hires_components, aes(xmin = position, xmax = position + 1,
+                                             ymin = 1, ymax = 2), alpha = hires_components[["n_2_alpha"]], fill = color_green) +
+      geom_rect(data = hires_components, aes(xmin = position, xmax = position + 1,
+                                             ymin = 2, ymax = 3), alpha = hires_components[["n_3_alpha"]], fill = color_blue) +
+      geom_rect(data = edgy_classes, aes(xmin = position, xmax = position + 1,
+                                         ymin = 0, ymax = 3), fill = edgy_classes[["color"]]) +
+      geom_rect(data = no_coverage, aes(xmin = position, xmax = position + 1,
+                                        ymin = 0, ymax = 3), fill = color_grey) +
+      theme_bw() +
+      labs(title = "Assigned class components on sequence",
+           x = "Position",
+           y = "") +
+      theme(axis.ticks.y = element_blank(),
+            axis.text.y = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank()) +
+      coord_cartesian(x = c(0, protein_length+1))
+
+  } else {
+
+    hires_components <- hires_params %>%
+      filter(is.na(class_name), !is.na(n_1)) %>%
+      mutate(n = n_1 + n_2 + n_3,
+             n_1_color = rgb(n_1/n, 0, 0),
+             n_2_color = rgb(0, n_2/n, 0),
+             n_3_color = rgb(0, 0, n_3/n))
+
+    ggplot() +
+      geom_rect(data = hires_components, aes(xmin = position, xmax = position + 1,
+                                             ymin = 0, ymax = 1), fill = hires_components[["n_1_color"]]) +
+      geom_rect(data = hires_components, aes(xmin = position, xmax = position + 1,
+                                             ymin = 1, ymax = 2), fill = hires_components[["n_2_color"]]) +
+      geom_rect(data = hires_components, aes(xmin = position, xmax = position + 1,
+                                             ymin = 2, ymax = 3), fill = hires_components[["n_3_color"]]) +
+      geom_rect(data = edgy_classes, aes(xmin = position, xmax = position + 1,
+                                         ymin = 0, ymax = 3), fill = edgy_classes[["color"]]) +
+      geom_rect(data = no_coverage, aes(xmin = position, xmax = position + 1,
+                                        ymin = 0, ymax = 3), fill = color_grey) +
+      theme_bw() +
+      labs(title = "Assigned class components on sequence",
+           x = "Position",
+           y = "") +
+      theme(axis.ticks.y = element_blank(),
+            axis.text.y = element_blank(),
+            panel.grid.major = element_blank(),
+            panel.grid.minor = element_blank()) +
+      coord_cartesian(x = c(0, protein_length+1))
+
+
+  }
 
 
 }
