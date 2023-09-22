1649365586e66887d4e6cdf62fb1c91fe6416d73
Weronika Puchała <ciemnamateria@gmail.com>
2023-09-22 14:11:25 +0200

    uc curves for two state fit and peptide uc difference plot added

--- R/two_states.R
+++ R/two_states.R
@@ -67,3 +67,122 @@ plot_color_distance <- function(two_state_dataset){
     coord_cartesian(x = c(0, protein_length+1))
 
 }
+
+
+#'
+#' @importFrom ggplot2 geom_segment
+#'
+#'
+#' @export plot_uc_distance
+
+plot_uc_distance <- function(kin_dat_1,
+                             kin_dat_2,
+                             fractional = T){
+
+
+  peptide_list <- merge(kin_dat_1, kin_dat_2, by = c("Protein", "Sequence", "Start", "End")) %>%
+    select(Sequence, Start, End) %>%
+    unique(.) %>%
+    arrange(Start, End)
+
+  protein_length <- max(peptide_list[["End"]])
+
+  res <- lapply(1:nrow(peptide_list), function(i){
+
+    fit_dat_1 <- filter(kin_dat_1,
+                        Sequence == peptide_list[["Sequence"]][i],
+                        Start == peptide_list[["Start"]][i],
+                        End == peptide_list[["End"]][i])
+    fit_dat_2 <- filter(kin_dat_2,
+                        Sequence == peptide_list[["Sequence"]][i],
+                        Start == peptide_list[["Start"]][i],
+                        End == peptide_list[["End"]][i])
+
+    get_uc_distance(fit_dat_1, fit_dat_2)
+
+  }) %>% bind_rows()
+
+  if (fractional){
+    ggplot(res) +
+      geom_segment(aes(x = Start, xend = End, y = frac_uptake_diff, yend = frac_uptake_diff)) +
+      labs(title = "Fractional uptake difference",
+           y = "diff value",
+           x = "Position") +
+      coord_cartesian(x = c(0, protein_length+1))
+
+  } else {
+
+    ggplot(res) +
+      geom_segment(aes(x = Start, xend = End, y = uptake_diff, yend = uptake_diff)) +
+      labs(title = "Uptake difference",
+           y = "diff value",
+           x = "Position") +
+      coord_cartesian(x = c(0, protein_length+1))
+  }
+
+}
+
+
+
+#'
+#'
+#' @export get_uc_distance
+#'
+get_uc_distance <- function(fit_dat_1,
+                            fit_dat_2){
+
+  merge(fit_dat_1, fit_dat_2, by = c("ID", "Protein", "MaxUptake", "Sequence", "Start", "End", "Exposure")) %>%
+    mutate(tmp_frac_uptake_diff = ((frac_deut_uptake.x - frac_deut_uptake.y)/(err_frac_deut_uptake.x + err_frac_deut_uptake.y))^2,
+           tmp_uptake_diff = ((deut_uptake.x - deut_uptake.y)/(err_deut_uptake.x + err_deut_uptake.y))^2) %>%
+    arrange(Exposure) %>%
+    group_by(ID, Protein, MaxUptake, Sequence, Start, End) %>%
+    summarize(frac_uptake_diff = sum(tmp_frac_uptake_diff),
+              uptake_diff = sum(tmp_uptake_diff))
+
+}
+
+
+#'
+#'
+#' @export plot_uc
+
+plot_uc <- function(fit_dat_1,
+                    fit_dat_2,
+                    fit_values_1,
+                    fit_values_2){
+
+  v_sequence = fit_dat_1[["Sequence"]][1]
+  v_start = fit_dat_1[["Start"]][1]
+  v_end = fit_dat_1[["End"]][1]
+
+  fit_params_1 <- fit_values_1 %>%
+    filter(sequence == v_sequence,
+           start == v_start,
+           end == v_end)
+
+  fit_params_2 <- fit_values_2 %>%
+    filter(sequence == v_sequence,
+           start == v_start,
+           end == v_end)
+
+  ggplot() +
+    geom_point(data = fit_dat_1, aes(x = Exposure, y = frac_deut_uptake, shape = State)) +
+    # geom_line(data = fit_dat_1, aes(x = Exposure, y = frac_deut_uptake, linetype = State)) +
+    geom_point(data = fit_dat_2, aes(x = Exposure, y = frac_deut_uptake, shape = State)) +
+    # geom_line(data = fit_dat_2, aes(x = Exposure, y = frac_deut_uptake, linetype = State)) +
+    scale_x_log10() +
+    ylim(c(0, 1.25)) +
+    theme(legend.position = "bottom") +
+    stat_function(fun=function(x){fit_params_1[["n_1"]]*(1-exp(-fit_params_1[["k_1"]]*x)) + fit_params_1[["n_2"]]*(1-exp(-fit_params_1[["k_2"]]*x)) + fit_params_1[["n_3"]]*(1-exp(-fit_params_1[["k_3"]]*x))}, linetype = "dashed") +
+    stat_function(fun = function(x){fit_params_1[["n_1"]]*(1-exp(-fit_params_1[["k_1"]]*x))}, color = "red", linetype = "dashed") +
+    stat_function(fun = function(x){fit_params_1[["n_2"]]*(1-exp(-fit_params_1[["k_2"]]*x))}, color = "green", linetype = "dashed") +
+    stat_function(fun = function(x){fit_params_1[["n_3"]]*(1-exp(-fit_params_1[["k_3"]]*x))}, color = "blue", linetype = "dashed") +
+    stat_function(fun=function(x){fit_params_2[["n_1"]]*(1-exp(-fit_params_2[["k_1"]]*x)) + fit_params_2[["n_2"]]*(1-exp(-fit_params_2[["k_2"]]*x)) + fit_params_2[["n_3"]]*(1-exp(-fit_params_2[["k_3"]]*x))}, linetype = "solid") +
+    stat_function(fun = function(x){fit_params_2[["n_1"]]*(1-exp(-fit_params_2[["k_1"]]*x))}, color = "red", linetype = "solid") +
+    stat_function(fun = function(x){fit_params_2[["n_2"]]*(1-exp(-fit_params_2[["k_2"]]*x))}, color = "green", linetype = "solid") +
+    stat_function(fun = function(x){fit_params_2[["n_3"]]*(1-exp(-fit_params_2[["k_3"]]*x))}, color = "blue", linetype = "solid") +
+    labs(x = "Exposure [min]",
+         y = "Fractional DU [%]",
+         title = paste0("Deuterium uptake curve for peptide ", v_sequence, " (", v_start, "-", v_end, ")"))
+
+}
--- R/utils.R
+++ R/utils.R
@@ -1,4 +1,4 @@
 #' @export
-get_peptide_list <- function(kin_dat){
-  unique(select(kin_dat, Sequence, Start, End))
+get_peptide_list <- function(dat){
+  arrange(unique(select(dat, Sequence, Start, End)), Start, End)
 }
