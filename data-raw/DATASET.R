9403237f45edb74e58f461ad846ee19765508e18
Weronika Puchała <ciemnamateria@gmail.com>
2024-05-22 16:12:37 +0200

    data moved and example improved

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,5 +3,8 @@
 gamma_dat <- HaDeX::read_hdx("C:/Users/User/Downloads/GAMMAalpha_cut.csv.csv")
 alpha_dat <- HaDeX::read_hdx("C:/Users/User/Desktop/article_hradex/ALPHA G i BG.csv")
 
+alpha_dat <- HRaDeX::move_dataset(alpha_dat, -25)
+
 usethis::use_data(alpha_dat, overwrite = TRUE)
 usethis::use_data(gamma_dat, overwrite = TRUE)
+
